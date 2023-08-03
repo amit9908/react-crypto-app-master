f292bddfd454ec5ef4d2765e73ef664724d9e4b8
Amit <ak2512099@gmail.com>
2023-08-03 18:59:21 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
 
         <VStack>
           <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
-          <Text>Our Founder</Text>
+          <Text></Text>
         </VStack>
       </Stack>
     </Box>
